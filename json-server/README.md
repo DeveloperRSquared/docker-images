ece2025bbe7ef01b47e7204ec648360d248946d9
rikhilrai <rikhilrai@gmail.com>
2020-12-29 15:34:24 +0000

    feature: add json-server (#1)
    * feature: add json-server
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .

---
+++ json-server/README.md
@@ -0,0 +1,3 @@
+# json-server
+
+JSON server docker image
---
+++ json-server/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+args="$@ --port 8000 --host 0.0.0.0"
+
+file=/app/data/db.json
+if [ -f $file ]; then
+    args="$args $file"
+fi
+
+file=/app/data/routes.json
+if [ -f $file ]; then
+    args="$args --routes $file"
+fi
+
+json-server $args
