be11330fc28d71cf5b7ed5615359a57659553b90
Rikhil Raithatha <rikhil@upsidetechnology.co>
2020-12-30 13:01:18 +0000

    feature: add json-server-node (#4)

--- json-server/run.sh
+++ json-server/run.sh
@@ -6,17 +6,17 @@ args="$@ --port 8000 --host 0.0.0.0"
 volume_path=/app/data
 
 db_file=${volume_path}/db.json
-if [ -f ${db_file} ]; then
+if [[ -f ${db_file} ]]; then
     args="${args} ${db_file}"
 fi
 
 routes_file=${volume_path}/routes.json
-if [ -f ${routes_file} ]; then
+if [[ -f ${routes_file} ]]; then
     args="${args} --routes ${routes_file}"
 fi
 
 middlewares_directory=${volume_path}/middlewares
-if [ -d "${middlewares_directory}" ]; then
+if [[ -d "${middlewares_directory}" ]]; then
     middlewares=""
     for middleware in $( ls ${middlewares_directory}/*.js ); do
         middlewares+="${middleware} "
